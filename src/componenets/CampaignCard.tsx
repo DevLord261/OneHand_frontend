ea9e5493a7c3c1b331390d60d42178feba24a7aa
DevLord261 <husseinbayoume@proton.me>
2025-05-09 17:50:39 +0300

    progress

--- src/componenets/CampaignCard.tsx
+++ src/componenets/CampaignCard.tsx
@@ -12,23 +12,25 @@ export default function CampaignCard() {
         alt="solar energy"
         loading="lazy"
       />
-      <div>
-        <img src={Verified} alt="verfied badge"></img>
-        <p>Verified</p>
-      </div>
-      <h3 className={styles.title} style={{ paddingLeft: "20px" }}>
-        Solar Energy Campaign
-      </h3>
-      <div style={{ width: "100%", padding: "20px" }}>
-        <ProgressBar value={75} /> {/* 75% funded */}
-      </div>
-      <div className={styles.info}>
-        <p>$10,000 Raised</p>
-        <p>Goal: $20,000</p>
-      </div>
-      <div>
-        <p>Location: New York</p>
-      </div>
+      <section className={styles.sectioncontainer}>
+        <div>
+          <img src={Verified} alt="verfied badge"></img>
+          <p>Verified</p>
+        </div>
+        <h3 className={styles.title} style={{ paddingLeft: "20px" }}>
+          Solar Energy Campaign
+        </h3>
+        <div style={{ width: "100%", padding: "20px" }}>
+          <ProgressBar value={75} /> {/* 75% funded */}
+        </div>
+        <div className={styles.info}>
+          <p>$10,000 Raised</p>
+          <p>Goal: $20,000</p>
+        </div>
+        <div>
+          <p>Location: New York</p>
+        </div>
+      </section>
     </div>
   );
 }
