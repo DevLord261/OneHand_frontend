71c9664a5a707bc106a3641985f12fcbcfcdbbfa
DevLord261 <husseinbayoume@proton.me>
2025-05-25 23:45:25 +0300

    disable footer and header in Auth page

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,42 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router";
 import Home from "./pages/Home.tsx";
 import Login from "./pages/Login.tsx";
+import Campaign from "./pages/ViewCampaign.tsx";
+import ToolBar from "./componenets/ToolBar.tsx";
+
+function App() {
+  const location = useLocation();
+  const noFooterRoutes = ["/login"];
+  const hideFooter = noFooterRoutes.includes(location.pathname);
+
+  return (
+    <>
+      {!hideFooter && (
+        <nav>
+          <ToolBar />
+        </nav>
+      )}
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/campaign:id" element={<Campaign />} />
+      </Routes>
+      {!hideFooter && (
+        <footer>
+          <p>© 2025 OneHand</p>
+          <a>Terms</a> <a>Privacy</a> <a>Notice</a> <a>Legal</a>
+          <a>Accessibility</a> <a>Statement</a> <a>Cookie</a> <a>Policy</a>
+        </footer>
+      )}
+    </>
+  );
+}
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <App />
   </BrowserRouter>,
 );
